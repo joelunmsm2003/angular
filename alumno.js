9481d1eecb3c3795d708ac719c0cf155ab2f7233
byte <joelunmsm@gmail.com>
2014-12-13 15:48:22 -0500

    angular

--- alumno.js
+++ alumno.js
@@ -1,10 +1,29 @@
+
+
 function AlumnoController($scope){
 
 	$scope.alumno = [
 
-	{nombre: "Juan Blanco",curso: "3 ESO"},
+	{nombre: "zuan Blanco",curso: "3 ESO"},
 	{nombre: "Juan Blanco1",curso: "3 ESO"},
 	{nombre: "Juan Blanco2",curso: "3 ESO"}
 
 	];
-}	
+	$scope.Save=function() {
+
+	$scope.alumno.push({nombre:$scope.nuevoAlumno.nombre,curso:$scope.nuevoAlumno.curso})
+	$scope.FormVisibility=false;
+	console.log($scope.FormVisibility)
+	}	
+
+	$scope.FormVisibility=false;
+
+	$scope.ShowForm=function(){
+
+		$scope.FormVisibility=true;
+		console.log($scope.FormVisibility)
+	}
+}
+
+
+
