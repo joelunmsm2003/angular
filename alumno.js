c6bccf1e6d947fe332707462b7cb74204619bb7d
byte <joelunmsm@gmail.com>
2014-12-13 13:42:48 -0500

    angular

---
+++ alumno.js
@@ -0,0 +1,10 @@
+function AlumnoController($scope){
+
+	$scope.alumno = [
+
+	{nombre: "Juan Blanco",curso: "3 ESO"},
+	{nombre: "Juan Blanco1",curso: "3 ESO"},
+	{nombre: "Juan Blanco2",curso: "3 ESO"}
+
+	];
+}	
